80c21a63cc01d580382a8532538639ed19d2274d
kittiwin <kittiwinktb@gmail.com>
2023-03-07 16:14:11 +0700

    lab24complete

--- lab24.cpp
+++ lab24.cpp
@@ -28,6 +28,65 @@ ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 	return ComplexNumber(real-c.real,imag-c.imag);
 }
 
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	return ComplexNumber((real*c.real)-(imag*c.imag),(real*c.imag)+(imag*c.real));
+}
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	double R = 0, I = 0;
+	R = ((real*c.real)+(imag*c.imag))/(pow(c.real,2)+pow(c.imag,2));
+	I = ((imag*c.real)-(real*c.imag))/(pow(c.real,2)+pow(c.imag,2));
+	return ComplexNumber(R, I);
+}
+
+ComplexNumber operator+(double s ,const ComplexNumber &c){
+	return ComplexNumber(s+c.real,c.imag);
+}
+
+ComplexNumber operator-(double s ,const ComplexNumber &c){
+	return ComplexNumber(s-c.real,-c.imag);
+}
+
+ComplexNumber operator*(double s, const ComplexNumber &c){
+	return ComplexNumber(s*c.real,s*c.imag);
+}
+
+ComplexNumber operator/(double s, const ComplexNumber &c){
+	double real, image;
+	real = (s*c.real)/(pow(c.real,2)+pow(c.imag,2));
+	image = (s*-1*c.imag)/(pow(c.real,2)+pow(c.imag,2));
+	return ComplexNumber(real, image);
+}
+
+double ComplexNumber::angle(){
+	double Angle;
+	Angle = atan(imag/real)*180/3.14159 ;
+	if(real < 0 && imag > 0) Angle = 180 + Angle;
+	else if(real < 0 && imag < 0) Angle = Angle - 180;
+	return Angle;
+}
+
+ostream & operator<<(ostream &os, const ComplexNumber &c){
+	if(c.imag == 0) return os << c.real;
+	else if(c.real == 0) return os << c.imag << "i";
+	else if(c.imag > 0) return os << c.real<< "+" << c.imag << "i";
+	else if(c.imag < 0) return os << c.real<< c.imag << "i";
+	else return os << c.real;
+}
+
+bool ComplexNumber::operator==(const ComplexNumber &c){
+	if(real == c.real && imag == c.imag) return true;
+	else return false;
+}
+
+bool operator==(double s,const ComplexNumber &c){
+	if(s == c.real && 0 == c.imag) return true;
+	else return false;
+}
+
+double ComplexNumber::abs(){
+	return sqrt(pow(real,2)+pow(imag,2));
+}
 //Write your code here
 
 int main(){
